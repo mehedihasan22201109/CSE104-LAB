c9ac5235b13013c4799464c8720191c8d6ed10d2
Saswata Ghosal <140143035+SaswataGA@users.noreply.github.com>
2023-08-09 08:02:47 +0600

    Add files via upload

---
+++ Leap year  in while loop.c
@@ -0,0 +1,31 @@
+
+
+#include<stdio.h>
+int main()
+{
+   int m;
+   printf("Enter a year:");
+   scanf("%d",&m);
+   while(1)
+    {
+    if(m%100 == 0){
+        if(m%400 == 0){
+            printf("leap year");
+            break;
+        }
+        else{
+         break
+         }
+    }
+    else if (m%4 == 0){
+        printf("leap year");
+        break;
+    }
+   else{
+   break;
+  }
+    }
+
+
+    return 0;
+}
