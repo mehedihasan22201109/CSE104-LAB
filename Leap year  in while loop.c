e4197255ae1894d82932f8a4389a88769450f218
Saswata Ghosal <140143035+SaswataGA@users.noreply.github.com>
2023-08-09 08:19:34 +0600

    Leap year  in while loop.c

--- Leap year  in while loop.c
+++ Leap year  in while loop.c
@@ -14,6 +14,7 @@ int main()
             break;
         }
         else{
+            printf("Not Leap Year");
          break
          }
     }
@@ -22,6 +23,7 @@ int main()
         break;
     }
    else{
+       printf("Not Leap Year");
    break;
   }
     }
